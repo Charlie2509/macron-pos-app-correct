87fdcd16e116f5309c8e5bc826f89ff5635af969
Charlie2509 <charlie.southgate@macron.com>
2026-04-14 15:14:39 +0100

    Add macron POS ping route and pre-intent reachability check

---
+++ app/routes/api.macron-pos.ping.jsx
@@ -0,0 +1,11 @@
+export const action = async ({ request }) => {
+  console.log('[MSH-PING] ROUTE HIT', request.method);
+  return Response.json({ ok: true, pong: true }, {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+  });
+};
