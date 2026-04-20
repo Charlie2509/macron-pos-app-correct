0df9895a9e60b6975271c48240f3b9677e249b05
Charlie2509 <charlie.southgate@macron.com>
2026-04-20 16:25:09 +0100

    Update shopify.d.ts

--- extensions/macron-pos/shopify.d.ts
+++ extensions/macron-pos/shopify.d.ts
@@ -7,6 +7,12 @@ declare module './src/GiftCardTile.jsx' {
 }
 
 //@ts-ignore
+declare module './src/Tile.jsx' {
+  const shopify: import('@shopify/ui-extensions/pos.home.tile.render').Api;
+  const globalThis: { shopify: typeof shopify };
+}
+
+//@ts-ignore
 declare module './src/GiftCardModal.jsx' {
   const shopify: import('@shopify/ui-extensions/pos.home.modal.render').Api;
   const globalThis: { shopify: typeof shopify };
